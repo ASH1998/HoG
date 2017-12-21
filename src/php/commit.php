c506d07859d52240fbeba833cf3019b58d1d213f
David <Dpynes@gmail.com>
2017-12-20 22:53:01 -0800

    refreq#027: updates to db design.

--- src/php/commit.php
+++ src/php/commit.php
@@ -1,9 +1,22 @@
 <?php
+
   $post = file_get_contents('php://input');
-  var_dump($post);
-  /*
-    $p_classifier = $_POST['p_classifier'];
-    $p_featureVector = $_POST['p_featureVector'];
-    echo "hello world";
-  */
+  $data = json_decode($post);
+  $classifier = $data->p_classifier;
+  $feature = $data->p_featureVector;
+  
+  try {
+    define('DB','mysql:host=localhost;dbname=HoG');
+    define('USER','HoGUser');
+    define('PASS','hog4fun');
+    $con = new PDO( DB,USER,PASS,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION) );
+  } catch(Exception $e) {
+      die(0);
+  }
+
+  $stmt = $con->prepare("CALL add_a_feature_and_class(:classifier, :feature);");
+  $stmt->bindValue(':classifier',strval($classifier),PDO::PARAM_STR);
+  $stmt->bindValue(':feature',strval($feature),PDO::PARAM_STR);
+  $stmt->execute();
+
 ?>
